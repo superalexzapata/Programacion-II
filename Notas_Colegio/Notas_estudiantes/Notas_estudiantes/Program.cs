36f119a0faf052a2fdc93cf326005566fb958271
superalexzapata <alexzapata0115@gmail.com>
2022-08-22 22:59:56 -0500

    inicio

---
+++ Notas_Colegio/Notas_estudiantes/Notas_estudiantes/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Notas_estudiantes // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
