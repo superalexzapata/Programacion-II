e74bf06d41da6b9456f43d15932dae135a0b9744
superalexzapata <alexzapata0115@gmail.com>
2022-08-26 22:11:12 -0500

    Promedio notas

--- Notas_Colegio/Notas_estudiantes/Notas_estudiantes/Program.cs
+++ Notas_Colegio/Notas_estudiantes/Notas_estudiantes/Program.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Notas_estudiantes // Note: actual namespace depends on the project name.
+namespace Prueba
 {
-    public class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-
             string[] Estudiantes;
-            int[] Notas = null;
+            double[] Notas = null;
             int Cant_Nombres;
             int Cant_notas;
             double promedio_notas = 0;
-            int suma =0;
+            double suma = 0;
 
             Console.Write("Cuántos estudiantes hay en clase?: ");
             Cant_Nombres = int.Parse(Console.ReadLine());
             Estudiantes = new string[Cant_Nombres];
-            
+            Notas = new double[Cant_Nombres];
+
             for (int i = 0; i < Cant_Nombres; i++)
             {
                 Console.Write("Nombre estudiante " + (i + 1) + ": ");
@@ -29,30 +23,28 @@ namespace Notas_estudiantes // Note: actual namespace depends on the project nam
 
                 Console.Write("Cuántas notas tiene " + Estudiantes[i] + " ?:");
                 Cant_notas = int.Parse(Console.ReadLine());
-                Notas = new int[Cant_notas];
-                
+                double nota = 0;
+                suma = 0;
+
                 for (int a = 0; a < Cant_notas; a++)
                 {
                     Console.Write("Nota " + (a + 1) + ": ");
-                    Notas[a] = Convert.ToInt32(Console.ReadLine());
-                    suma += Notas[a]; 
-                    promedio_notas = suma / Cant_notas;
+                    nota = Convert.ToDouble(Console.ReadLine());
+                    suma += nota;
+                    
                 }
-
+                promedio_notas = suma / Cant_notas;
+                Notas[i] = promedio_notas;
             }
 
             Console.WriteLine("Lista de Estudiantes:");
 
             for (int i = 0; i < Cant_Nombres; i++)
             {
-                Console.WriteLine("{0}. {1}",i+1 + Estudiantes[i]);
-                /*for (int j = 0; j < promedio_notas; j++)
-                {
-                    Console.WriteLine("{0}. {1}", i + 1 + Notas[j]);
-                }
-                Console.WriteLine("");*/
+                Console.WriteLine("La nota Promedio del Estudiante " + Estudiantes[i] + " Es: " + Notas[i] );
+                
             }
-            
+
             Console.Read();
         }
     }
